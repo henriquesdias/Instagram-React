938be716a801ce96ec5f3d79e7121ba89b2d1cd8
henriquesdias <henrique.dias1915@gmail.com>
2022-07-14 23:36:10 -0300

    layout of header finished

--- src/index.js
+++ src/index.js
@@ -1,10 +1,4 @@
 import ReactDom from 'react-dom';
+import App from './Components/App';
 
-function App(){
-    return (
-        <div>
-            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Commodi tempore dolor explicabo sequi expedita quam quas, culpa aliquid magnam fugit possimus perferendis quaerat suscipit! Reprehenderit optio earum necessitatibus laborum quisquam!</p>
-        </div>
-    );
-}
-ReactDom.render(App() , document.querySelector('.root'));
+ReactDom.render(<App /> , document.querySelector('.root'));
